3c0747f9c2f5d495e12a0c3ad6bfc8f8b26c2a2f
Squeshly-Freezed <251692768+Squeshly-Freezed@users.noreply.github.com>
2026-03-16 21:14:40 +1300

    Finish basic GameController, display Gameboard.

--- script.js
+++ script.js
@@ -1,1 +1,81 @@
-console.log("hello");
+function Gameboard() {
+    const array = [];
+    for (let i = 0; i < 9; i++) {
+        array.push(Cell());
+    }
+
+    const getBoard = () => array.map(index => index.getValue());
+
+    const placeMarker = (player, spot) => {
+        if (array[spot] === 0) array[spot].addValue(player);
+    };
+
+    const printBoard = () => {
+        console.log(`${getBoard()}`); //try to add 2 line breaks to display proper board.
+    };
+
+    return { 
+        getBoard,
+        placeMarker,
+        printBoard,
+    };
+}
+
+function GameController() {
+    const board = Gameboard();
+    const player1 = Player("Rob", 1);
+    const player2 = Player("AI", 2);
+
+    let activePlayer = player1;
+
+    const getActivePlayer = () => activePlayer;
+
+    const switchPlayerTurn = () => {
+        activePlayer = activePlayer === player1 ? player2 : player1;
+    }
+
+    const printNewRound = () => {
+        board.printBoard();
+        console.log(`${getActivePlayer().name}'s Turn`);
+    }
+
+    const playRound = (spot) => {
+        console.log("Placing marker");
+        board.placeMarker(getActivePlayer().symbol, spot);
+        switchPlayerTurn();
+        printNewRound();
+    }
+
+    printNewRound();
+
+    return {
+        getActivePlayer,
+        playRound,
+    }
+}
+
+function Player(name, symbol) {
+    const score = 0;
+    const draws = 0;
+    return {
+        name,
+        symbol,
+        score,
+        draws,
+    };
+}
+
+function Cell(player) {
+    const value = 0;
+
+    const addValue = () => value = player;
+    const getValue = () => value;
+
+    return {
+        addValue,
+        getValue,
+    };
+}
+
+const game = GameController();
+game.playRound();
